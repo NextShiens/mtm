885e505d595364d484f55e8cdb3758b32e3a6458
Muhammad Fida Hassan <bc220411491mfh@vu.edu.pk>
2024-09-09 11:45:17 +0500

    feat: Add Success Stories screen and details screen
    - Create SuccessStories.js and SuccessStoriesDetals.js files in the Main/SuccessStories directory.
    - Implement the SuccessStories screen with a list of success story cards fetched from the server.
    - Add navigation functionality to navigate to the SuccessStoriesDetals screen when a success story card is pressed.

---
+++ app-matrimonial-main/src/screens/Main/SuccessStories/SuccessStories.js
@@ -0,0 +1,149 @@
+import {
+    View,
+    Text,
+    StyleSheet,
+    FlatList,
+    TouchableOpacity,
+    ScrollView,
+  } from 'react-native';
+  import React, { useEffect } from 'react';
+  import {useState} from 'react';
+  import SuccessStoriesCard from '../../../components/successStories/SuccessStoriesCard';
+  import {Image} from 'react-native';
+  import AsyncStorage from '@react-native-async-storage/async-storage';
+  import { useNavigation } from '@react-navigation/native';
+  import { API_URL } from '../../../../constant';
+  
+  
+  
+  export default function SuccessStories() {
+    const nav = useNavigation();
+    const [successStories, setSuccessStories] = useState([]);
+    const [newUsersLoading, setNewUsersLoading] = useState(true);
+  
+  
+  
+  
+    useEffect(() => { fetchSuccesStories(); }, []);
+  
+  
+    const fetchSuccesStories = async () => {
+      debugger
+      try {
+        setNewUsersLoading(true);
+        const token = await AsyncStorage.getItem('AccessToken');
+        const response = await fetch(`${API_URL}/user/get-success-stories`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        });
+  
+        if (!response.ok) {
+          if (response.status === 404) {
+            throw new Error('No new users found');
+          } else {
+            throw new Error('Something went wrong');
+          }
+        }
+  
+        const {successStories} = await response.json();
+        console.log('stories', successStories);
+        setSuccessStories(successStories);
+      } catch (error) {
+        console.error('Error fetching Success stories:', error);
+        throw error;
+      } finally {
+        setNewUsersLoading(false);
+      }
+    };
+    const arr = [
+      {
+        id: 1,
+        image: require('../../../assets/images/image.png'),
+        name: 'Lorem ipsum dolor',
+        decription:
+          'Lorem ipsum dolor sit amet consectetur. Risus nulla auctor tellus quam enim amet lorem. Consectetur aliquet nunc nunc.',
+      },
+      {
+        id: 1,
+        image: require('../../../assets/images/image.png'),
+        name: 'Lorem ipsum dolor',
+        decription:
+          'Lorem ipsum dolor sit amet consectetur. Risus nulla auctor tellus quam enim amet lorem. Consectetur aliquet nunc nunc.',
+      },
+      {
+        id: 1,
+        image: require('../../../assets/images/image.png'),
+        name: 'Lorem ipsum dolor',
+        decription:
+          'Lorem ipsum dolor sit amet consectetur. Risus nulla auctor tellus quam enim amet lorem. Consectetur aliquet nunc nunc.',
+      },
+      {
+        id: 1,
+        image: require('../../../assets/images/image.png'),
+        name: 'Lorem ipsum dolor',
+        decription:
+          'Lorem ipsum dolor sit amet consectetur. Risus nulla auctor tellus quam enim amet lorem. Consectetur aliquet nunc nunc.',
+      },
+      {
+        id: 1,
+        image: require('../../../assets/images/image.png'),
+        name: 'Lorem ipsum dolor',
+        decription:
+          'Lorem ipsum dolor sit amet consectetur. Risus nulla auctor tellus quam enim amet lorem. Consectetur aliquet nunc nunc.',
+      },
+    ];
+    return (
+      <View style={style.container}>
+        <View style={style.flexrow}>
+          <TouchableOpacity onPress={() => {nav.goBack()}}>
+            <Image source={require('../../../assets/images/leftarrow.png')} />
+          </TouchableOpacity>
+          <Text style={style.heading}>Recent Success Stories</Text>
+        </View>
+        <ScrollView contentContainerStyle={style.scrollContent}>
+          <FlatList
+            data={successStories}
+            renderItem={({item, index}) => (
+              <SuccessStoriesCard
+                {...item}
+                index={index}
+                image={item.image||require('../../../assets/images/leftarrow.png')}
+                name={item.title}
+                des={item.description}
+                onPress={() => {nav.navigate('SuccessStoriesDetals')}}
+              />
+            )}
+          />
+        </ScrollView>
+      </View>
+    );
+  }
+  
+  const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#F5F5F5'
+    },
+    flexrow: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: 20,
+      alignSelf: 'center',
+      marginBottom: 10,
+    },
+    heading: {
+      fontSize: 16,
+      color: 'black',
+      textAlign: 'center',
+      width: '85%',
+      fontWeight: '700',
+    },
+    scrollContent: {
+      paddingHorizontal: 10,
+      flex:1
+    },
+  });
+  
